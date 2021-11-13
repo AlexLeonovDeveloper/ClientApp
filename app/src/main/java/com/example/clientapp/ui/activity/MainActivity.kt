7ecd3edf79cd7db045e7857e2921267e6368cc43
Алексей <report-123@mail.ru>
2021-11-13 21:43:13 +0300

    add fragments, setting bottom navigations, theme.xml, color.xml

--- app/src/main/java/com/example/clientapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/clientapp/ui/activity/MainActivity.kt
@@ -2,12 +2,38 @@ package com.example.clientapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.clientapp.R
+import com.example.clientapp.databinding.ActivityMainBinding
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.clientHomeFragment, R.id.clientServicesFragment,
+            R.id.clientSearchFragment, R.id.clientChatFragment, R.id.clientProfileFragment
+        ).build()
+
+        binding.apply {
+            nav_bottom.setupWithNavController(navController)
+        }
     }
 }
