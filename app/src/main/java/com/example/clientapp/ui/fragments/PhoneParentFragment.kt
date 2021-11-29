4fa013f1540042ef003d804f7f67e2d0767eb6d4
Алексей <report-123@mail.ru>
2021-11-29 10:54:53 +0300

    edit app to manifest

---
+++ app/src/main/java/com/example/clientapp/ui/fragments/PhoneParentFragment.kt
@@ -0,0 +1,33 @@
+package com.example.clientapp.ui.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.clientapp.R
+import com.example.clientapp.viewmodels.ParentViewModel
+
+class PhoneParentFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PhoneParentFragment()
+    }
+
+    private lateinit var viewModel: ParentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.parent_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ParentViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/clientapp/viewmodels/ParentViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.clientapp.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class ParentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
